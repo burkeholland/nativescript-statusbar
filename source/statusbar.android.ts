9720ad84b50969832d33b5708df843539e8d9a52
Brad Martin <bradwaynemartin@gmail.com>
2016-02-17 20:24:06 -0600

    android working

--- source/statusbar.android.ts
+++ source/statusbar.android.ts
@@ -1,11 +1,20 @@
-// NOT IMPLEMENTED YET
 import common = require("./statusbar-common");
 import definition = require("statusbar");
+import app = require("application");
+import color = require("color");
 
 export class StatusBar extends common.StatusBar {
     
-    public update(value: string) {        
-        // TODO        
+    public update(value: string) {
+        try {
+            if (value) {
+                var nativeColor = new color.Color(value).android;
+                var window = app.android.startActivity.getWindow();
+                window.setStatusBarColor(nativeColor);
+            }        
+        } catch (err) {
+            console.log(err);
+        }        
     }
     
     constructor(options?: definition.Options) {
