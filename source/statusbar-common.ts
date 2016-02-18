bdd65c26f1b89053df894ff01a609951d4bb1b57
Brad Martin <bradwaynemartin@gmail.com>
2016-02-17 20:26:20 -0600

    android working

--- source/statusbar-common.ts
+++ source/statusbar-common.ts
@@ -1,57 +1,53 @@
-import definition = require("statusbar");
-import { View } from "ui/core/view";
-import * as dependencyObservable from "ui/core/dependency-observable";
-import * as proxy from "ui/core/proxy";
-import app = require("application");
-
-let BARSTYLE = "barStyle",
-    STATUSBAR = "StatusBar";
-    
-enum BarStyle {
-    default,
-    light,
-    dark,
-    opaque
-}
-    
-let onBarStylePropertyChanged = (data: dependencyObservable.PropertyChangeData) => {
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var view_1 = require("ui/core/view");
+var dependencyObservable = require("ui/core/dependency-observable");
+var proxy = require("ui/core/proxy");
+var app = require("application");
+var BARSTYLE = "barStyle", STATUSBAR = "StatusBar";
+var BarStyle;
+(function (BarStyle) {
+    BarStyle[BarStyle["default"] = 0] = "default";
+    BarStyle[BarStyle["light"] = 1] = "light";
+    BarStyle[BarStyle["dark"] = 2] = "dark";
+    BarStyle[BarStyle["opaque"] = 3] = "opaque";
+})(BarStyle || (BarStyle = {}));
+var onBarStylePropertyChanged = function (data) {
     try {
-        let statusbar = <StatusBar>data.object;
-        let value = data.newValue;
-        
+        var statusbar_1 = data.object;
+        var value = data.newValue;
         if (app.ios) {
-            setTimeout(() => { statusbar.update(BarStyle[value]); });
+            setTimeout(function () { statusbar_1.update(BarStyle[value]); });
         }
-
         if (app.android) {
-            setTimeout(() => { statusbar.update(value); });
+            setTimeout(function () { statusbar_1.update(value); });
         }
     }
     catch (err) {
         console.log(err);
     }
-}
-
-export class StatusBar extends View implements definition.StatusBar {  
-    
-    public static  barStyleProperty = new dependencyObservable.Property(
-        BARSTYLE,
-        STATUSBAR,
-        new proxy.PropertyMetadata(undefined, dependencyObservable.PropertyMetadataSettings.None, onBarStylePropertyChanged)
-    );
-    
-    constructor(options?: definition.Options) {
-        super(options);
+};
+var StatusBar = (function (_super) {
+    __extends(StatusBar, _super);
+    function StatusBar(options) {
+        _super.call(this, options);
     }
-    
-    public update(value: string) {
-        
-    }
-    
-    get barStyle(): any {
-        return this._getValue(StatusBar.barStyleProperty);
-    }
-    set barStyle(value: any) {
-        this._setValue(StatusBar.barStyleProperty, value);
-    }
-}
+    StatusBar.prototype.update = function (value) {
+    };
+    Object.defineProperty(StatusBar.prototype, "barStyle", {
+        get: function () {
+            return this._getValue(StatusBar.barStyleProperty);
+        },
+        set: function (value) {
+            this._setValue(StatusBar.barStyleProperty, value);
+        },
+        enumerable: true,
+        configurable: true
+    });
+    StatusBar.barStyleProperty = new dependencyObservable.Property(BARSTYLE, STATUSBAR, new proxy.PropertyMetadata(undefined, dependencyObservable.PropertyMetadataSettings.None, onBarStylePropertyChanged));
+    return StatusBar;
+})(view_1.View);
+exports.StatusBar = StatusBar;
