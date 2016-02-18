d0971033c91f7efb775577a0da5d08da34c3ce20
Brad Martin <bradwaynemartin@gmail.com>
2016-02-17 20:46:44 -0600

    added platform sdk check, doesn't work pre-lollipop

--- source/statusbar.android.ts
+++ source/statusbar.android.ts
@@ -1,5 +1,6 @@
 import common = require("./statusbar-common");
 import definition = require("statusbar");
+import platform = require("platform");
 import app = require("application");
 import color = require("color");
 
@@ -7,7 +8,7 @@ export class StatusBar extends common.StatusBar {
     
     public update(value: string) {
         try {
-            if (value) {
+            if (value && platform.device.sdkVersion >= "21") {
                 var nativeColor = new color.Color(value).android;
                 var window = app.android.startActivity.getWindow();
                 window.setStatusBarColor(nativeColor);
