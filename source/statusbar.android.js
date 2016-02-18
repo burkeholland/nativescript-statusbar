41fe4276462704882a43b6eee0b30dbf592993bd
Brad Martin <bradwaynemartin@gmail.com>
2016-02-17 20:25:34 -0600

    android working

--- source/statusbar.android.js
+++ source/statusbar.android.js
@@ -4,12 +4,24 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 var common = require("./statusbar-common");
+var app = require("application");
+var color = require("color");
 var StatusBar = (function (_super) {
     __extends(StatusBar, _super);
     function StatusBar(options) {
         _super.call(this, options);
     }
     StatusBar.prototype.update = function (value) {
+        try {
+            if (value) {
+                var nativeColor = new color.Color(value).android;
+                var window = app.android.startActivity.getWindow();
+                window.setStatusBarColor(nativeColor);
+            }
+        }
+        catch (err) {
+            console.log(err);
+        }
     };
     return StatusBar;
 })(common.StatusBar);
