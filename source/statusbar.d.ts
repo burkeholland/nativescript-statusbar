89e0dee51d623f147bf1a376b15b329c80636400
Arnav Gupta <dev@championswimmer.in>
2018-07-12 02:28:20 +0530

    update typedefinitions

--- source/statusbar.d.ts
+++ source/statusbar.d.ts
@@ -1,39 +1,39 @@
 /**
  * Contains the StatusBar class, which allows setting of the status bar style on iOS via markup.
  */
-declare module "statusbar" {
+declare module "nativescript-statusbar" {
 
-    import { View } from "ui/core/view";
+  import { View } from "tns-core-modules/ui/core/view";
 
-    /**
-     * Represents a StatusBar widget.
-     */
-    export class StatusBar extends View {
-        /*
-        * Updates the style of the status bar to the supplied value
-        */
-        updateBarStyle(value: string);
+  /**
+   * Represents a StatusBar widget.
+   */
+  export class StatusBar extends View {
+    /*
+    * Updates the style of the status bar to the supplied value
+    */
+    updateBarStyle(value: string): void;
 
-        /*
-        * Updates the color of the status bar to the supplied value
-        */
-        updateBarColor(value: string);
+    /*
+    * Updates the color of the status bar to the supplied value
+    */
+    updateBarColor(value: string): void;
 
-        android: any /* android.widget.VideoView */;
+    android: any /* android.widget.VideoView */;
 
-        ios: any /* AVPlayerViewController */;
-    }
+    ios: any /* AVPlayerViewController */;
+  }
 
-    export interface Options {
+  export interface Options {
 
-        /*
-        * Gets or sets the style of the status bar
-        */
-        barStyle: string;
+    /*
+    * Gets or sets the style of the status bar
+    */
+    barStyle: string;
 
-        /*
-        * Gets or sets the color of the status bar
-        */
-        barColor: string;
-    }
+    /*
+    * Gets or sets the color of the status bar
+    */
+    barColor: string;
+  }
 }
